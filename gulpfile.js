44ec618627351160cb880f31ce31666e3b9e818e
Jeremy Wadhams <jwadhams@dealerinspire.com>
2016-11-02 16:18:35 -0500

    Add an eslint config and update the code to match it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp = require('gulp'),
-    exec = require('child_process').exec;
- 
-gulp.task('test', function(cb) {
-    exec(
-		'node testrunner.js',
-		{ cwd : 'tests' },
-		function (err, stdout, stderr) {
-			console.log(stdout);
-			console.log(stderr);
-			cb(err);
-		}
-	);
+var gulp = require("gulp");
+var exec = require("child_process").exec;
+
+gulp.task("test", function(cb) {
+  exec(
+    "node testrunner.js",
+    {cwd: "tests"},
+    function(err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    }
+  );
 });
 
-gulp.task('default', function() {
-    gulp.watch(['**/*.js', 'tests/tests.json'], ['test']);
+gulp.task("default", function() {
+  gulp.watch(["**/*.js", "tests/tests.json"], ["test"]);
 });
