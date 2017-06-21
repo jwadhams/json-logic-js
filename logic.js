2b9322b33efe0c9b36adcd62ea0a29783ee31aab
Jeremy Wadhams <jwadhams@dealerinspire.com>
2017-06-21 14:32:48 -0500

    Special case for 'Cannot read property 'Something' of null' issues

--- logic.js
+++ logic.js
@@ -110,6 +110,9 @@ http://ricostacruz.com/cheatsheets/umdjs.html
       var sub_props = String(a).split(".");
       var data = this;
       for(var i = 0; i < sub_props.length; i++) {
+        if(data === null){
+            return not_found;
+        }
         // Descending into data
         data = data[sub_props[i]];
         if(data === undefined) {
