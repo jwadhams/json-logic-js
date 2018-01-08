977efd99458836be5aeaf1e216e91afe04cd2990
German Meza <iam@germanamz.com>
2018-01-08 16:22:34 -0600

    Added typings for typescript

---
+++ logic.d.ts
@@ -0,0 +1,8 @@
+export declare class jsonLogic {
+  private static is_logic(): boolean;
+  private static truthy(): any;
+  static apply(): any;
+  private static uses_data(): any;
+  static add_operation(): void;
+  private static rule_like(): boolean;
+}
