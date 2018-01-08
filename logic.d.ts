ff200a5b27dd7f39ab6c175b66288747393046c3
German Meza <iam@germanamz.com>
2018-01-08 16:27:54 -0600

    Fixed the definition

--- logic.d.ts
+++ logic.d.ts
@@ -1,8 +1,4 @@
 export declare class jsonLogic {
-  private static is_logic(): boolean;
-  private static truthy(): any;
-  static apply(): any;
-  private static uses_data(): any;
-  static add_operation(): void;
-  private static rule_like(): boolean;
+  static apply(logic: any, data: any): any;
+  static add_operation(name: string, code: Function): void;
 }
