1518cb3f33c024f5475904196e4cc84418678806
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2019-02-22 15:55:14 +0100

    refactore: build

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,11 @@ const uglify = require('rollup-plugin-uglify');
 const optimize = require('rollup-plugin-optimize-js');
 const filesize = require('rollup-plugin-filesize');
 
-module.exports = {
+module.exports = [{
   input: 'src/index.js',
   plugins: [
     resolve(),
     babel(),
-    // uglify.uglify(),
-    // optimize(),
     filesize({ showMinifiedSize: false }),
   ],
   output: {
@@ -19,4 +17,19 @@ module.exports = {
     name: 'jsonLogic',
     exports: 'default',
   }
-};
+}, {
+  input: 'src/index.js',
+  plugins: [
+    resolve(),
+    babel(),
+    uglify.uglify(),
+    optimize(),
+    filesize({ showMinifiedSize: false }),
+  ],
+  output: {
+    file: 'dist/jsonLogic.min.js',
+    format: 'umd',
+    name: 'jsonLogic',
+    exports: 'default',
+  }
+}];
