875f7946df2fd8b833305d63d6bf3e09e45f5214
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2019-03-07 11:19:34 +0100

    fix: thruthy renamed to  notnot

--- src/operations/logic/index.js
+++ src/operations/logic/index.js
@@ -1,6 +1,6 @@
 export { default as equal } from './equal';
 export { default as not } from './not';
 export { default as notEqual } from './notEqual';
+export { default as notnot } from './notnot';
 export { default as strictEqual } from './strictEqual';
 export { default as strictNotEqual } from './strictNotEqual';
-export { default as truthy } from './truthy';
